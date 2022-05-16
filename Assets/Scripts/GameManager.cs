86d0672e7dabb7aa4973cd3ec94fb9619f537506
sm42499 <89489372+sm42499@users.noreply.github.com>
2022-05-16 15:44:36 -0700

    CwC 5 completed, need to fix small mistakes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
@@ -10,26 +13,30 @@ public class GameManager : MonoBehaviour
     private int score;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+       
     }
 
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
     }
         IEnumerator SpawnTarget()
         {
-            while (true)
+            while (isGameActive)
             {
                 yield return new WaitForSeconds(spawnRate);
                 int index = Random.Range(0, targets.Count);
                 Instantiate(targets[index]);
+                
                 
             }
         }
@@ -46,6 +53,21 @@ public class GameManager : MonoBehaviour
         {
 
         }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    public void StartGame()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        isGameActive = true;
+        titleScreen.gameObject.SetActive(false);
+    }
 }
+
+
+
